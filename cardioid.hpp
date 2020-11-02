e0fa94e09689dea65afab1b5fc14cfa9397e885a
asakhar <a@sakhar.ru>
2020-11-02 17:09:49 +0300

    setter fixed

--- cardioid.hpp
+++ cardioid.hpp
@@ -14,12 +14,16 @@ private:
 
 public:
   Cardioid(double a = 1) {
-    if(a < 0)
+    if (a < 0)
       throw std::runtime_error("'a' cant be less then zero.");
     _a = a;
   }
   double const &getA() const;
-  double &setA(double const &val) { return _a = val; }
+  double &setA(double const &val) {
+    if (val < 0)
+      throw std::runtime_error("'a' cant be less then zero.");
+    return _a = val;
+  }
   double getR(double const &angle) const { return 2 * _a * (1 - cos(angle)); }
   double getMaxLengthFromAxis() const { return 3 * _a; }
   double getArea() const { return 6 * pi<double> * pow(_a, 2); }
